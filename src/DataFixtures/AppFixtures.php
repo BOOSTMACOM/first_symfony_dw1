c3eaee164938f47ec54a8e79f65863509415ca02
Théo <boostmacom@gmail.com>
2023-05-25 09:44:03 +0200

    Initial commit

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Post;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        for($i=0;$i<500;$i++)
+        {
+            $post = new Post();
+            $post->setTitle('Article ' . $i);
+            $post->setContent('Contenu de mon premier article');
+            $post->setCreatedAt(new \DateTimeImmutable());
+
+            $manager->persist($post);
+        }
+        
+        $manager->flush();
+    }
+}
