3fb7cf40fb6fd03b534881bb27404105770999d6
Théo <boostmacom@gmail.com>
2023-06-01 09:27:53 +0200

    Début du crud pour les catégories

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryFormType;
 use App\Repository\CategoryRepository;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,6 +21,33 @@ class CategoryController extends AbstractController
         ]);
     }
 
+    #[Route('/category/new', name: 'app_category_new')]
+    public function new(Request $request, CategoryRepository $categoryRepository)
+    {
+        // On construit le formulaire basé sur un type
+        $form = $this->createForm(CategoryFormType::class);
+
+        // On alimente le formulaire avec les données envoyées dans la requête
+        $form->handleRequest($request);
+
+        // On vérifie que le formualire est bien envoyé
+        if($form->isSubmitted())
+        {
+            // On recupère les données du formulaire dans un variable
+            $category = $form->getData();
+
+            // On enregistre les données qui sont sous forme d'entité dans la base via le repository
+            $categoryRepository->save($category, true);
+
+            // On redirige vers la page des catégories
+            return $this->redirectToRoute('app_category');
+        }
+
+        return $this->render('category/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
     #[Route('/category/{id}', name: 'app_category_show')]
     public function show(int $id, CategoryRepository $categoryRepository): Response
     {
@@ -24,4 +55,6 @@ class CategoryController extends AbstractController
             'category' => $categoryRepository->find($id),
         ]);
     }
+
+
 }
